f18a01961793da3705abcbf17806b86f500ef158
howard611 <howard.tam.95@gmail.com>
2016-03-30 16:25:31 +0800

    Enable file uploading

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -4,19 +4,58 @@ $(document).ready(function() {
     return matches ? matches[1] : null;
   }
 
+  function get_signed_request(file) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/sign_s3?file_name=' + file.name + '&file_type=' + file.type);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          upload_file(file, response.signed_request, response.url);
+        } else {
+          alert("Could not get signed URL.");
+        }
+      }
+    };
+    xhr.send();
+  }
+
+  function upload_file(file, signed_request, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', signed_request);
+    xhr.setRequestHeader('x-amz-acl', 'public-read');
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        document.getElementById('resumeLink').value = url;
+        $('#signupForm').submit();
+      }
+    };
+    xhr.onerror = function() {
+      alert('Could not upload file.');
+    };
+    xhr.send(file);
+  }
+
   $('button').click(function(evt) {
     evt.preventDefault();
 
     if (window.location.hash) {
       var access_token = getHashValue('access_token');
       var getUrl = 'https://my.mlh.io/api/v1/user?access_token=' + access_token;
+      var files = document.getElementById("inputResume").files;
+      var file = files[0];
 
       $.get(getUrl, function(data) {
         $('input#id').val(data.data.id);
-        $('#signupForm').submit();
+        if (file === null) {
+          $('#signupForm').submit();
+        } else {
+          get_signed_request(file);
+        }
       });
+
     } else {
-      console.log('Cannot read user ID from the access_token. Please try again.');
+      alert('Missing access_token. Please try again.');
     }
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var Applicant = require('../models/applicant.js');
+var aws = require('aws-sdk');
+var AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+var S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'OxfordHack' });
+});
+
+router.get('/sign_s3', function(req, res) {
+  aws.config.update({ accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY });
+  var s3 = new aws.S3();
+  var s3_params = {
+    Bucket: S3_BUCKET_NAME,
+    Key: req.query.file_name,
+    ContentType: req.query.file_type,
+    ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3_params, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var return_data = {
+        signed_request: data,
+        url: 'https://' + S3_BUCKET_NAME + '.s3.amazonaws.com/' + req.query.file_name
+      };
+      res.write(JSON.stringify(return_data));
+      res.end();
+    }
+  });
 });
 
 router.get('/success', function(req, res, next) {
@@ -19,7 +46,7 @@ router.post('/success', function(req, res) {
     linkedin: fields.linkedin,
     twitter: fields.twitter,
     github: fields.github,
-    resume: fields.resume
+    resume: fields.resumeLink
   });
 
   applicant.save(function(err) {
